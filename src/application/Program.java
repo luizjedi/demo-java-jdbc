c8d1387ddeeb714f835b277ea9e8e88ec12b2c91
luizjedi <luiz.pepe@hotmail.com>
2021-05-06 00:33:01 -0300

    Implements findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,19 @@
 package application;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "LUIZ", "luiz@gmail.com", new Date(), 5700.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
 				
 		System.out.println(seller);
-		
+
 	}
 
 }
